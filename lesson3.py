a29ee20f1bc35f1c3d499fed0035e39684fdbad6
student <verbenkopasa@gmail.com>
2022-10-29 13:18:56 +0300

    зроблений опис сіма

---
+++ lesson3.py
@@ -0,0 +1,9 @@
+import random
+class Human:
+    def __init__(self, name = "Sim", job = None, home = None, car = None):
+        self.name = name
+        self.job = job
+        self.home = home
+        self.car = car
+        self.gladness = 50
+        self.stiety = 50
